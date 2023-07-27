21cda31af63f49ab17d1133a91f3ea1193faa951
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-27 16:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const apiUrls = [
 ];
 
 // You can write your code here
+
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+const fetchAllData = async () => {
+  const start = performance.now();
+  const results = await Promise.all(apiUrls.map(fetchData));
+  const end = performance.now();
+  document.getElementById('output-all').innerHTML = `Time taken: ${end - start}ms`;
+};
+
+const fetchAnyData = async () => {
+  const start = performance.now();
+  const result = await Promise.any(apiUrls.map(fetchData));
+  const end = performance.now();
+  document.getElementById('output-any').innerHTML = `Time taken: ${end - start}ms`;
+};
+
+fetchAllData();
+fetchAnyData();
